ed0d4a4ed0bc866b7d194fb20c46a762a08f849c
pooja2443 <pooja.p@solutelabs.com>
2025-04-16 10:20:00 +0530

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-// App.js - Main entry point
+
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
